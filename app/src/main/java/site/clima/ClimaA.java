c4bd89ec929da7287142ded534064bf624a35f44
Leidy Johana León Muñoz <lleon@sitesas.co>
2016-04-26 10:13:38 -0500

    Adición de vista para landscape. Adición de list.

--- app/src/main/java/site/clima/ClimaA.java
+++ app/src/main/java/site/clima/ClimaA.java
@@ -1,52 +1,70 @@
 package site.clima;
 
-import android.content.Intent;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-
-import java.text.DateFormat;
-import java.util.Date;
-
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ClimaA extends AppCompatActivity {
 
-   // private TextView fecha;
-    //private Date fecha = '21/4/16';
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clima);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceHolderFragment())
+                    .commit();
+        }
     }
 
-    @Override
-    protected void onStart(){
-        super.onStart();
-    }
+    public static class PlaceHolderFragment extends Fragment {
 
-    @Override
-    protected void onRestart(){
-        super.onRestart();
-    }
+        public PlaceHolderFragment() {}
 
-    @Override
-    protected void onResume(){
-        super.onResume();
-    }
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
 
-    @Override
-    protected void onPause(){
-        super.onPause();
-    }
+            ArrayAdapter<String> mForecastAdapter;
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
+            String[] data = {
+                    "Mon 6/23 - Sunny - 31/17",
+                    "Tue 6/24 - Foggy - 21/8",
+                    "Wed 6/25 - Cloudy - 22/17",
+                    "Thurs 6/26 - Rainy - 18/11",
+                    "Fri 6/27 - Foggy - 21/10",
+                    "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                    "Sun 6/29 - Sunny - 20/7"
+            };
 
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
-    }
 
+            List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+            mForecastAdapter = new ArrayAdapter<String>(
+                    getActivity(),
+                    R.layout.list_item_forecast, // Nuevo layout
+                    R.id.list_item_forecast_textview, // Contenedor con la información
+                    weekForecast
+            );
+
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(mForecastAdapter);
+            return rootView;
+        }
+
+    }
 }
+
+
+
+
+
+
