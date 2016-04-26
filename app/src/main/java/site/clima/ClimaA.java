5479d7085a0edbd019869910b1a7ee2258274ddf
Leidy Johana León Muñoz <lleon@sitesas.co>
2016-04-26 07:57:36 -0500

    Segundo git

---
+++ app/src/main/java/site/clima/ClimaA.java
@@ -0,0 +1,52 @@
+package site.clima;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+
+public class ClimaA extends AppCompatActivity {
+
+   // private TextView fecha;
+    //private Date fecha = '21/4/16';
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clima);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+    }
+
+}
